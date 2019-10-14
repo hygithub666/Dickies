ee7111d669ac12fedc9dcd6476efd7f94567a1d4
hygithub666 <1349422319@qq.com>
2019-10-14 17:54:32 +0800

    第一版

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp =require("gulp");
+
+
+
+//监听任务
+gulp.task("watchall",async()=>{
+    //监听html,进行复制
+    gulp.watch("*.html",async()=>{
+        gulp.src("*.html")
+        .pipe(gulp.dest("D:\\phpStudy\\WWW\\DickiesBig"))
+    })
+})
