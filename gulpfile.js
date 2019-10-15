505abbb400a3c2898f62eaf6ff98f3a5f0096158
hygithub666 <1349422319@qq.com>
2019-10-15 12:06:21 +0800

    10月15日上午

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 const gulp =require("gulp");
 const sass =require("gulp-sass");
+
 const uglify =require("gulp-uglify");
 
-
+//自动生成sass编译后文件
+gulp.task("sass",function(){
+    return gulp.src("sass/**/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("D:\\phpStudy\\WWW\\DickiesBig"));
+});
 
 
 //监听任务
@@ -13,11 +19,15 @@ gulp.task("watchall",async()=>{
         .pipe(gulp.dest("D:\\phpStudy\\WWW\\DickiesBig"))
     })
 
+    gulp.watch("css/*.css",async ()=>{
+        gulp.src("css/*.css")
+        .pipe(gulp.dest("D:\\phpStudy\\WWW\\DickiesBig\\css"));
+    });
     //监听sass文件
-    gulp.watch("sass/**/*",async()=>{
-        gulp.src("sass/**/*")
+    gulp.watch("sass/**/*.scss",async()=>{
+        gulp.src("sass/**/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("D:\\phpStudy\\WWW\\DickiesBig\\css"))
+        .pipe(gulp.dest("D:\\Dickies\\css"));
     })
 
     //监听js文件
@@ -25,6 +35,7 @@ gulp.task("watchall",async()=>{
         gulp.src("js/*.js")
         .pipe(uglify())
         .pipe(gulp.dest("D:\\phpStudy\\WWW\\DickiesBig\\js"));
-	});
+    });
+    
 	
 })
